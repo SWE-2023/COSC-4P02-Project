6fe2abfcd5812fdb207026491a87585dbe443e55
monwe-jr <monwefrancis@gmail.com>
2023-02-14 21:17:33 -0500

    Add svelte component file for timeline items

---
+++ timeline/src/components/TimeLineItem.svelte
@@ -0,0 +1,16 @@
+<!--for handing data components individually-->
+<script>
+    export let item 
+</script>
+
+<li class = lineItem>
+    <span class = "title">{item.title}</span>
+    <span class = "dot"></span>
+    <span class = "date">{item.id}</span>
+</li>
+
+<style>
+  
+  
+   
+</style>
