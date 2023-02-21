562a761a90130aef337f79b521fea56e90f241b5
monwe-jr <monwefrancis@gmail.com>
2023-02-21 02:29:20 -0500

    Fix positioning

--- timeline/src/components/TimeLineItem.svelte
+++ timeline/src/components/TimeLineItem.svelte
@@ -5,29 +5,27 @@
 </script>
 
 <div style="padding-top: {spacing}px" class = lineItem>
-    <span class = "dot"></span>
-    <span class = "date">{item.id}</span>
+    <div class = "dot"></div>
+    <div class = "date">{item.id}</div>
 </div>
 
 <style>
     .lineItem {
         display: flex;
-        justify-content: left;
-        position: relative;
-        right: 5px;
+        position: absolute;
+        left:42px;
     }
     
     .dot {
-        width: 15px;
-        height: 15px;
+        width: 17px;
+        height: 17px;
         background-color: brown;
         border-radius: 20%;
-        border-radius: 138px; 
+        border-radius: 100px; 
     }
 
     .date {
-        padding-left: 10px;
-        
+        padding-left: 10px;  
     }
    
 </style>
