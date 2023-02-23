c91308f6ccfdc53c572ffb856b94dcacfb105797
monwe-jr <monwefrancis@gmail.com>
2023-02-23 16:20:56 -0500

    Add event functions to implement item change

--- timeline/src/components/TimeLineItem.svelte
+++ timeline/src/components/TimeLineItem.svelte
@@ -1,24 +1,22 @@
 <!--for handing data components individually-->
 <script>
+  	import {createEventDispatcher} from 'svelte';
     import Dot from './Dot.svelte';
     export let item; 
     export let spacing;
-    export let currentTitle;
-    export let currentPicture;
-    export let currentText;
+    export let currentItem;
 
+    const dispatch = createEventDispatcher();
+    const change = () => dispatch('change');
     function setDetails(){
-		console.log("test");
-        currentTitle = item.title;
-        currentPicture = item.picture;
-        currentText = item.text; 
+      currentItem = item; 
     }
-
+    
 </script>
 
 <div class="lineItem">
 	<div style="top:{spacing}px">
-	  <Dot onClick={setDetails} isActive={false}/>
+	  <Dot eventOne={setDetails} eventTwo={change} isActive={false}/>
 	  <div class="date">{item.id}</div>
 	</div>
   </div>
