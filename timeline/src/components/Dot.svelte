483e3fca7be9e56f3d34cad44b453d9128d231fb
Alec Ames <dev@alecames.com>
2023-02-22 23:21:00 -0500

    Fixed button scale and click events

--- timeline/src/components/Dot.svelte
+++ timeline/src/components/Dot.svelte
@@ -1,31 +1,51 @@
 <script>
-
+	export let onClick;
+	export let isActive = false;
+	
+	function handleClick() {
+		isActive = !isActive; 
+		onClick(isActive);
+	}
 	
 </script>
 
-<button class="dot"> </button>
+<button on:click={handleClick} class:active={isActive}></button>
 
 <style>
-    .dot {
-        width: 17px;
-        height: 17px;
-        background-color: brown;
+    button {
+        width: 18px;
+        height: 18px;
+        background-color: var(--color-theme-1);
         border-radius: 100%; 
-        border:none;
+        border: solid 2px transparent;
+		transform-origin: center;
+		transition:all 0.1s ease-in-out;
     }
 
-    .dot:hover {
-        cursor:pointer;
+	button:hover {
+		cursor:pointer;
+	}
+
+	button:hover:not(.active) {
+		transform:scale(1.2);
     }
 
-    .dot:active {
-        width: 25px;
-        height: 25px;
-        background-color: white;
+	button:hover.active {
+		transform:scale(1.4);
+	}
+	
+    button:active {
+		transform:scale(1.5);
+        background-color: var(--color-bg-1);
         border-radius: 100%;
-        border-style: solid;
-        border-color: brown;
-    }
-    
-    
+        border: solid 2px var(--color-theme-1);
+    } 
+	
+	.active {
+		background-color: var(--color-bg-1);
+        border-radius: 100%;
+        border: solid 2px var(--color-theme-1);
+        transform:scale(1.5);
+	}
+	
 </style>
