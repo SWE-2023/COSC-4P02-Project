7ccfd3f26364f18da8b45355b4c38592895c7a04
Alec Ames <dev@alecames.com>
2023-02-23 03:44:24 -0500

    Modified Dot component

--- timeline/src/components/Dot.svelte
+++ timeline/src/components/Dot.svelte
@@ -2,27 +2,17 @@
 	export let onClick;
 </script>
 
-<button on:click={onClick}></button>
+<button style="" on:click={onClick}></button>
 
 <style>
     button {
         width: 18px;
         height: 18px;
         background-color: var(--color-theme-1);
-        border-radius: 100%; 
+        border-radius: 100vh; 
         border: solid 2px transparent;
 		transform-origin: center;
-		transition:all 0.1s ease-in-out;
+		transition:all 0.15s ease-in-out;
+		z-index:10;
     }
-
-	button:hover {
-		cursor:pointer;
-		transform:scale(1.2);
-	}
-
-    button:active {
-		transform:scale(1.1);
-        border: solid 2px var(--color-theme-1);
-    } 
-	
 </style>
