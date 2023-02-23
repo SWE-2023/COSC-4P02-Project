bc2d3cad84e7c137229d25e589fe98001c7e9588
Alec Ames <dev@alecames.com>
2023-02-23 00:31:00 -0500

    Removed toggle function

--- timeline/src/components/Dot.svelte
+++ timeline/src/components/Dot.svelte
@@ -1,15 +1,8 @@
 <script>
 	export let onClick;
-	export let isActive = false;
-	
-	function handleClick() {
-		isActive = !isActive; 
-		onClick(isActive);
-	}
-	
 </script>
 
-<button on:click={handleClick} class:active={isActive}></button>
+<button on:click={onClick}></button>
 
 <style>
     button {
@@ -24,28 +17,12 @@
 
 	button:hover {
 		cursor:pointer;
-	}
-
-	button:hover:not(.active) {
 		transform:scale(1.2);
-    }
-
-	button:hover.active {
-		transform:scale(1.4);
 	}
-	
+
     button:active {
-		transform:scale(1.5);
-        background-color: var(--color-bg-1);
-        border-radius: 100%;
+		transform:scale(1.1);
         border: solid 2px var(--color-theme-1);
     } 
-	
-	.active {
-		background-color: var(--color-bg-1);
-        border-radius: 100%;
-        border: solid 2px var(--color-theme-1);
-        transform:scale(1.5);
-	}
 	
 </style>
