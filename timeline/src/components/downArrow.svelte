9139c6c3ae97c2133941ede76a4da12dc4d72723
haarisyahya <haarisyahya@gmail.com>
2023-02-23 18:03:21 -0500

    Comments added for installing dependencies

--- timeline/src/components/downArrow.svelte
+++ timeline/src/components/downArrow.svelte
@@ -1,6 +1,11 @@
 <script>
-    import Fa from 'svelte-fa/src/fa.svelte';
-    import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+    import Fa from 'svelte-fa/src/fa.svelte'; //please type and run "npm install svelte-fa" in terminal
+
+    
+    import { faChevronDown } from '@fortawesome/free-solid-svg-icons'; 
+    //please type and run "npm install @fortawesome/free-solid-svg-icons" &
+    //"npm install @fortawesome/free-brands-svg-icons" in terminal to avoid any errors
+    
 </script>
     
     <div class = "lower">
