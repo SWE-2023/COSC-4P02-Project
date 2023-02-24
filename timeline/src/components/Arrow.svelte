17bf43d5bae076714560a250249a7186656fd3a4
Alec Ames <dev@alecames.com>
2023-02-23 23:58:29 -0500

    Fixed arrow element, snaps to top/bottom of screen

--- timeline/src/components/Arrow.svelte
+++ timeline/src/components/Arrow.svelte
@@ -1,62 +1,60 @@
 <script>
-  import {createEventDispatcher} from 'svelte';
-  import Fa from 'svelte-fa/src/fa.svelte';
-  import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
-  import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+	import { createEventDispatcher } from "svelte";
+	import Fa from "svelte-fa/src/fa.svelte";
+	import { faChevronUp } from "@fortawesome/free-solid-svg-icons";
+	import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 
-  export let alt = false;
-  export let upFunction; 
-  export let downFunction;
+	export let alt = false;
+	export let upFunction;
+	export let downFunction;
 
-  const dispatch = createEventDispatcher();
-  const goUp = () => dispatch('moveUp');
-  const goDown= () => dispatch('moveDown');
+	const dispatch = createEventDispatcher();
+	const goUp = () => dispatch("moveUp");
+	const goDown = () => dispatch("moveDown");
 </script>
-    
 
 {#if alt}
-  <div class = "down">
-    <button class = "down-button"  on:click={goDown} on:click={downFunction}>
-      <Fa icon={faChevronDown}  size = "2x"/>
-    </button>
-  </div>
+	<div class="down">
+		<button class="down-button" on:click={goDown} on:click={downFunction}>
+			<Fa icon={faChevronDown} size="2x" />
+		</button>
+	</div>
 {:else}
-  <div class = "up">
-    <button class = "up-button" on:click={goUp} on:click={upFunction}>
-    <Fa icon={faChevronUp} size = "2x"/>
-    </button>
-  </div>
+	<div class="up">
+		<button class="up-button" on:click={goUp} on:click={upFunction}>
+			<Fa icon={faChevronUp} size="2x" />
+		</button>
+	</div>
 {/if}
-  
- 
+
 <style>
-  .up {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: relative;
-    bottom: 5rem;
-  }
+	.up {
+		position: fixed;
+		left: 50%;
+		transform: translateX(-50%);
+		top: 2rem;
+    z-index:9999;
+	}
 
-  .down{
-    display: flex; 
-    justify-content: center;
-    align-items: center;
-    position: relative;
-    top: 10rem;
-  }
+	.down {
+		position: fixed;
+		left: 50%;
+		transform: translateX(-50%);
+		bottom: 2rem;
+    z-index:9999;
+	}
 
-  .up-button{
-    cursor: pointer;
-    background-color: transparent;
-    border:none;
-    padding: 0;      
-  }
-        
-  .down-button{
-    cursor: pointer;
-    background-color: transparent;
-    border:none;
-    padding: 0;
-  }
+	.up-button {
+		cursor: pointer;
+		background-color: transparent;
+		border: none;
+		padding: 0;
+	}
+
+	.down-button {
+		cursor: pointer;
+		background-color: transparent;
+		border: none;
+		padding: 0;
+	}
 </style>
