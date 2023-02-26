c0cf8a246ec1678bf0906ccdc9178b9d0ca66a71
Alec Ames <dev@alecames.com>
2023-02-26 03:46:51 -0500

    Testing Svelete transitions for TL items (...) - works but buggy, need to hit the button twice to get desired transition effect - configurable with different delays for experiementing

---
+++ timeline/src/components/ItemTransition.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { fly } from "svelte/transition";
+	export let direction;
+
+	let transition = fly;
+	let transitionInParams = { duration: 333, delay: 334 };
+	let transitionOutParams = { duration: 333 };
+	let offset = 500;
+	
+	$: null; 
+</script>
+
+<div
+	in:transition={{ 
+		...transitionInParams, 
+		x: 0, 
+		y: direction == "up" ? -offset : offset,
+		}}
+	out:transition={{
+		...transitionOutParams,
+		x: 0,
+		y: direction == "up" ? offset : -offset,
+	}}>
+	<slot />
+</div>
