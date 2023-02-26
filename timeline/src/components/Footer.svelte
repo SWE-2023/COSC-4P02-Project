480425da560ecf5f5ca759186d4022da33be8997
Alec Ames <dev@alecames.com>
2023-02-26 05:06:57 -0500

    Accessibility hotfix

--- timeline/src/components/Footer.svelte
+++ timeline/src/components/Footer.svelte
@@ -1,19 +1,40 @@
+<script>
+	import { countStore } from "../store.js";
+	import { page } from "$app/stores";
+	import PageTransitionFly from "./PageTransitionFly.svelte";
+
+	let countValue = 0;
+	countStore.subscribe((value) => {
+		countValue = value;
+	});
+</script>
+
 <footer>
-	<p>&copy; 2023</p>
+	<p>
+		<span
+			style="opacity:{countValue / 100 + 0.2};font-weight:{countValue *
+				100};color:{countValue >= 100
+				? 'var(--color-theme-1)'
+				: 'var(--color-text)'}">{countValue}</span>
+		{#if !$page.url.pathname.startsWith("/timeline")}
+			| <a href="https://www.notlmuseum.ca">NOTL Museum</a>
+			| <a href="https://www.notl.com">NOTL</a> |
+			<a href="https://www.niagararegion.ca">Niagara Region</a>
+		{/if}
+		| &copy; 2023
+	</p>
 </footer>
 
 <style>
 	footer {
+		position: fixed;
+		bottom: 0;
+		right: 0;
 		font-size: 0.8em;
 		display: flex;
-		flex-direction: column;
+		flex-direction: row;
 		justify-content: center;
 		align-items: flex-end;
 		padding: 0 16px;
 	}
-
-	/* 
-	footer a {
-		font-weight: bold;
-	} */
 </style>
--- timeline/src/store.js
+++ timeline/src/store.js
@@ -1,4 +1,11 @@
 import { writable } from 'svelte/store';
 
 export const reduceMotionStore = writable(false);
+export let count = writable(0);
 
+// make count store locally
+export const countStore = {
+	subscribe: count.subscribe,
+	set: count.set,
+	update: count.update,
+};
