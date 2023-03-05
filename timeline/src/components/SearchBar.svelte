7b30b3b1846d1f35870b624066896262e2ac0207
monwe-jr <monwefrancis@gmail.com>
2023-03-04 19:28:39 -0500

    Add SearchBar component

---
+++ timeline/src/components/SearchBar.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { faSearch } from "@fortawesome/free-solid-svg-icons";
+    import Fa from "svelte-fa/src/fa.svelte";
+    export let currentPage;
+</script>
+
+
+{#if currentPage == '../routes/timeline/+page.svlete'}
+    <div class="container">
+        <button class="icon">
+            <Fa icon={faSearch} size="21px"/>
+        </button>
+    </div>
+{/if}
+
+
+<style>
+    .container .icon{
+        position:absolute;
+        left: 30px;
+        top: 50%;
+    }
+</style>
