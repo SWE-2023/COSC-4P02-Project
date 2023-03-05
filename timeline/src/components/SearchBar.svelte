9410162434170576e5666b0754ff69520238df80
monwe-jr <monwefrancis@gmail.com>
2023-03-05 02:58:23 -0500

    Add style to search bar

--- timeline/src/components/SearchBar.svelte
+++ timeline/src/components/SearchBar.svelte
@@ -1,23 +1,52 @@
 <script>
     import { faSearch } from "@fortawesome/free-solid-svg-icons";
     import Fa from "svelte-fa/src/fa.svelte";
-    export let currentPage;
+    export let searchBarOpacity = 1;
+
 </script>
 
-
-{#if currentPage == '../routes/timeline/+page.svlete'}
-    <div class="container">
-        <button class="icon">
-            <Fa icon={faSearch} size="21px"/>
-        </button>
-    </div>
-{/if}
-
+<div class="container" style="opacity:{searchBarOpacity}">
+    <input type="text" placeholder="Search" class="search-box">
+    <button class="icon">
+        <Fa icon={faSearch} color="brown"/>
+    </button>
+</div>
 
 <style>
-    .container .icon{
-        position:absolute;
-        left: 30px;
-        top: 50%;
+    .container {
+        position: relative;
+        height: 30px;
+        border: 2px solid brown;
+        padding: 0px 10px;
+        border-radius: 50px;
+        display:flex;
+        flex-direction: row;
+        align-items: center;
+        background-color: white;
     }
+
+    .icon {
+        background-color: transparent;
+        border: none;
+    }
+
+    .icon:hover {
+        cursor: alias;
+    }
+
+    .search-box {
+        width: 200px;
+        border: none;
+        border-radius: 50px;
+        font-size: 15px;
+        color: brown;
+        font-family: var(--font-sans);
+    }
+
+    .search-box:focus {
+        outline:none;
+    }
+
+    
+   
 </style>
