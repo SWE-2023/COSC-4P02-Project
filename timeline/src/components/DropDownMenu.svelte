0f04ba99307d579639f5c666e160a578c35d5631
monwe-jr <monwefrancis@gmail.com>
2023-03-08 02:08:11 -0500

    Abstract drop down menu

---
+++ timeline/src/components/DropDownMenu.svelte
@@ -0,0 +1,35 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+    import DropDownItem from "./DropDownItem.svelte";
+    export let currentlyTitle;
+    export let list;
+    export let show;
+    const dispatch = createEventDispatcher();
+    const changeItem = () => dispatch("itemSelected");
+
+</script>
+
+{#if show}
+    {#if list.length > 0 }
+        {#each list as title}
+            <DropDownItem bind:selectedTitle={currentlyTitle} itemTitle={title} on:itemSelected={changeItem}/>
+        {/each}
+    {:else}
+        <div class="noResult">No Result!</div>		
+    {/if}	
+{/if}
+ 
+<style>
+    .noResult {
+        position: relative;
+        height: 30px;
+        width: 245px;
+        border: 2px solid black;
+        background-color: rgb(206, 204, 204);
+        display:flex;
+        align-items: center;
+        overflow: hidden;
+        text-overflow:ellipsis;
+    }
+
+</style>
