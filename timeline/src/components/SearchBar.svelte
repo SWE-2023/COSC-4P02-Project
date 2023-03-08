4eaf358a238e4d1b95bb9a897b69d938e2d40ae8
monwe-jr <monwefrancis@gmail.com>
2023-03-08 02:09:16 -0500

    Update component to handle events

--- timeline/src/components/SearchBar.svelte
+++ timeline/src/components/SearchBar.svelte
@@ -1,21 +1,45 @@
 <script>
+    import { createEventDispatcher } from "svelte";
+    import DropDownMenu from "./DropDownMenu.svelte";
     import { faSearch } from "@fortawesome/free-solid-svg-icons";
     import Fa from "svelte-fa/src/fa.svelte";
+    export let currentDropDownSelection;
     export let searchBarOpacity = 1;
+    export let titles;
+    let showResults = false; //to only show items when an input is given
+    let filtered = [];
+    let userInput = "";
+    const dispatch = createEventDispatcher();
+    const notify = () => dispatch("selectionMade");
+    const findTitles = () => { return filtered = titles.filter(item => item.toLowerCase().match(userInput.toLowerCase())); }
+
+    const showDropDown = () => {
+        if(userInput != ""){
+            showResults = true;
+        }else{
+            showResults = false;
+        }
+    }
 
 </script>
 
 <div class="container" style="opacity:{searchBarOpacity}">
-    <input type="text" placeholder="Search" class="search-box">
-    <button class="icon">
+    <input type="text" placeholder="Search" class="search-box" bind:value={userInput} on:input={showDropDown} on:input={findTitles}>
+    <div class="icon">
         <Fa icon={faSearch} color="brown"/>
-    </button>
+    </div>
 </div>
+<DropDownMenu 
+    bind:currentlyTitle={currentDropDownSelection} 
+    show={showResults} 
+    list={filtered} 
+    on:itemSelected={notify}/>		
 
 <style>
     .container {
         position: relative;
         height: 30px;
+        width: 220px;
         border: 2px solid brown;
         padding: 0px 10px;
         border-radius: 50px;
@@ -46,7 +70,4 @@
     .search-box:focus {
         outline:none;
     }
-
-    
-   
 </style>
