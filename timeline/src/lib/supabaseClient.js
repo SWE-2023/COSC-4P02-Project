dc6d9549507ec38e648becd2419206b7dc546659
Alec Ames <dev@alecames.com>
2023-03-30 17:40:59 -0400

    Removed unused fetch function

--- timeline/src/lib/supabaseClient.js
+++ timeline/src/lib/supabaseClient.js
@@ -4,14 +4,3 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 export default supabase;
-
-
-export async function fetch() {
-	const { data } = await supabase
-		.from("timeline")
-		.select("id, title, image, image_credit, body, start_date")
-		.order("start_date");
-	return {
-		body: data ?? [],
-	};
-}
