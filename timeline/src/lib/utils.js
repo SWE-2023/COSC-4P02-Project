190d9f53bdb4df7f608b8cc0e887266527fea874
Alec Ames <dev@alecames.com>
2023-04-16 22:08:38 -0400

    Styling and regex for email/phone num

---
+++ timeline/src/lib/utils.js
@@ -0,0 +1,9 @@
+export const validateEmail = (email) => {
+	const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(String(email).toLowerCase());
+};
+
+export const validatePhone = (phone) => {
+	const re = /^(\+\d{1,2}\s?)?1?-?\.?\s?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/;
+	return re.test(phone);
+};
--- timeline/src/routes/contact/+page.svelte
+++ timeline/src/routes/contact/+page.svelte
@@ -1,53 +1,59 @@
 <script>
+	// @ts-nocheck
+
 	import { slide } from "svelte/transition";
 	import PageTransition from "$lib/components/PageTransitionFly.svelte";
-    import Button from "$lib/components/Button.svelte";
-    import supabase from "$lib/supabaseClient";
-    import { toast } from "@zerodevx/svelte-toast";
+	import Button from "$lib/components/Button.svelte";
+	import supabase from "$lib/supabaseClient";
+	import { toast } from "@zerodevx/svelte-toast";
+	import { validatePhone, validateEmail } from "$lib/utils";
 
-	export let newInquiry = {
-		submitter_name:"",
-		phone:"",
-		email:"",
-		inquiryType:"",
-		message:""
+	export let inquiry = {
+		name: "",
+		phone: "",
+		email: "",
+		type: "",
+		message: "",
 	};
 
-	const submitInquiry =async () => {
-		if( newInquiry.submitterName.length != 0 && (newInquiry.phone.length != 0 || newInquiry.email.length != 0) && newInquiry.inquiryType.length != 0, newInquiry.message.length != 0){
+	const submitInquiry = async () => {
+		if (
+			Object.values(inquiry).every(
+				(value, index) => value.length != 0 || index === 1 || index === 2
+			)
+		) {
+			if (!validateEmail(inquiry.email)) {
+				toast.push("<b>Error</b><br>Please enter a valid email address.");
+				return;
+			}
+			if (!validatePhone(inquiry.phone)) {
+				toast.push("<b>Error</b><br>Please enter a valid phone number.");
+				return;
+			}
+			try {
+				const { data, error } = await supabase.from("inquiries").insert([
+					{
+						submitter_name: inquiry.name,
+						phone: inquiry.phone,
+						email: inquiry.email,
+						inquiry_type: inquiry.type,
+						message: inquiry.message,
+					},
+				]);
 
-			try{
-				const {error} = await supabase.from("inquiries").insert({
-					submitter_name: newInquiry.submitterName,
-					phone: newInquiry.phone,
-					email: newInquiry.email,
-					inquiryType: newInquiry.inquiryType,
-					message: newInquiry.message
-				});
-
-				if(error){
+				if (error) {
 					throw error;
 				}
 				toast.push("<b>Inquiry Submitted</b>");
-			}catch (error){
+			} catch (error) {
 				toast.push(`<b>Query Error</b><br>${error.message}`);
-				// toast.push("<b>Query Error</b><br>"+${error.message});
 			}
-		}else{
-			toast.push("<b>Error</b><br>Please fill out all fields before submitting.");
+		} else {
+			toast.push(
+				"<b>Error</b><br>Please fill out all fields before submitting."
+			);
 		}
-	}
-
-
-	// let newInquiry = {
-	// 	name:"",
-	// 	phone:"",
-	// 	email:"",
-	// 	inquiryType:"",
-	// 	message:""
-	// };
-
-
+	};
 </script>
 
 <svelte:head>
@@ -56,39 +62,40 @@
 </svelte:head>
 
 <PageTransition>
+	<h1 class="title">Contact Us</h1>
 
-    <h1>Contact Us</h1>
-                    
-    
-
-    <div class="container">
-        <div class="embedded-map">
-            <iframe loading="lazy" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11623.863995681377!2d-79.0716183!3d43.2521344!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xf975dbf3ff67d6c0!2sNiagara-on-the-Lake%20Museum!5e0!3m2!1sen!2sca!4v1614106500408!5m2!1sen!2sca" style="border:0;" width="600" height="450"></iframe>
-
+	<div class="container">
+		<div class="embedded-map">
+			<iframe
+				title="Map of Niagara-on-the-Lake Museum"
+				loading="lazy"
+				src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11623.863995681377!2d-79.0716183!3d43.2521344!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xf975dbf3ff67d6c0!2sNiagara-on-the-Lake%20Museum!5e0!3m2!1sen!2sca!4v1614106500408!5m2!1sen!2sca"
+				style="border:0;"
+				width="600"
+				height="450" />
 
 			<!-- ADDRESS -->
-			<div class="address-hours-container">
-				<div class="contact-flex-item address">
+			<div class="address-container">
+				<div class="contact-flex-item">
 					<p>
-						43 Castlereagh Street<br>
-						PO Box 208<br>
-						Niagara-on-the-Lake, ON<br>
-						L0S 1J0<br>
-						Canada<br>
+						43 Castlereagh Street<br />
+						PO Box 208<br />
+						Niagara-on-the-Lake, ON<br />
+						L0S 1J0<br />
+						Canada<br />
 					</p>
 				</div>
 
 				<!-- CONTACT INFO -->
-				<div class="contact-flex-item hours">
-					
+				<div class="contact-flex-item">
 					<table>
 						<tr>
 							<td>PHONE:</td>
-							<td><a href="tel:9054683912">905 468 3912</a></td>
+							<td><a href="tel:9054683912">(905) 468-3912</a></td>
 						</tr>
 						<tr>
 							<td>FAX:</td>
-							<td><a href="tel:9054681728">905 468 1728</a></td>
+							<td><a href="tel:9054681728">(905) 468-1728</a></td>
 						</tr>
 						<tr>
 							<td>EMAIL:</td>
@@ -97,134 +104,148 @@
 						<tr class="socials-row">
 							<td>SOCIALS:</td>
 							<td class="socials">
-								<a href="https://www.facebook.com/NOTLMuseum/" class="fa fa-facebook"></a>
-								<a href="https://twitter.com/NOTLMuseum" class="fa fa-twitter"></a>
-								<a href="https://www.instagram.com/notlmuseum/" class="fa fa-instagram"></a>
-								<a href="https://www.youtube.com/user/NOTLMuseum" class="fa fa-youtube-play"></a>
-							
+								<a href="https://www.facebook.com/NOTLMuseum/"
+									><span class="fa fa-facebook" /></a>
+								<a href="https://twitter.com/NOTLMuseum"
+									><span class="fa fa-twitter" /></a>
+								<a href="https://www.instagram.com/notlmuseum/"
+									><span class="fa fa-instagram" /></a>
+								<a href="https://www.youtube.com/user/NOTLMuseum"
+									><span class="fa fa-youtube-play" /></a>
 							</td>
 						</tr>
-
 					</table>
 				</div>
 			</div>
+		</div>
 
-
-        </div>
-
-        <div class="form-container">
-			<h2 id="inquiry-title">WE'D LOVE TO HEAR FROM YOU:</h2>
+		<div class="form-container">
+			<h2 id="inquiry-title">We'd love to hear from you:</h2>
 			<div class="row">
-				<form class="notify" action="/contact">
-					<div class="input col-xs-12 col-sm-6">
-						<input id="submitterName" class="text required" placeholder="Name" type="text" bind:value={newInquiry.submitterName}>
-					</div>
-					<div class="input col-xs-12 col-sm-6">
-						<input id="phone" class="text" placeholder="Phone" type="text" bind:value={newInquiry.phone}>
-					</div>
-					<div class="input col-xs-12 col-sm-6">
-						<input id="email" class="text required" placeholder="Email" type="text" bind:value={newInquiry.email}>
-					</div>
-					<div class="input col-xs-12 col-sm-6">
-						<input id="inquiryType" class="text" placeholder="Inquiry Type" type="text" bind:value={newInquiry.inquiryType}>
-					</div>
-					<div class="input col-xs-12">
-						<textarea id="message" class="text" cols="45" placeholder="Message" rows="8" bind:value={newInquiry.message}></textarea>
-					</div>
-					<div class="input submit col-xs-12">
-						<div id="submit-btn">
-							<Button alt on:click={submitInquiry} text="Submit" />
-						</div>
+				<form class="notify">
+					<input
+						id="name"
+						class="text required"
+						placeholder="Name"
+						type="text"
+						bind:value={inquiry.name} />
+					<input
+						id="phone"
+						placeholder="Phone"
+						type="text"
+						maxlength="10"
+						bind:value={inquiry.phone} />
+					<input
+						id="email"
+						class="text required"
+						placeholder="Email"
+						type="text"
+						bind:value={inquiry.email} />
+					<select id="type" bind:value={inquiry.type}>
+						<option value="" disabled selected>Select an inquiry type</option>
+						<option value="About the timeline">About the timeline</option>
+						<option value="About the museum">About the museum</option>
+						<option value="About the project">About Niagara-on-the-Lake</option>
+						<option value="Other">Other</option>
+					</select>
+					<textarea
+						id="message"
+						placeholder="Message"
+						bind:value={inquiry.message} />
+					<div id="submit-btn">
+						<Button alt on:click={submitInquiry} text="Submit" />
 					</div>
 				</form>
 			</div>
+		</div>
+	</div>
 
-        </div>
-        
-    </div>
-
-    
-    
-	<big-gap/>
+	<big-gap />
 </PageTransition>
 
 <style>
-	h1 {
+	* {
+		box-sizing: border-box;
+	}
+
+	.title {
 		text-align: center;
-        font-size: 80px;
 	}
 
-    iframe{
-        width:100%;
-    }
-
-	td{
-		height:2px;
-	}
-
-    .container{
-        display:flex;
-        flex-direction: row;
-        /* justify-content: center; */
-        align-content: center;
-    }
-    .embedded-map{
-        flex:1;
-    }
-
-	.contact-flex-item{
-		/* align-items: center; */
-		margin-top: 15px;
-		display: flex;
-		flex-wrap: nowrap;
-		font-size: 100%;
-	}
-
-	div.input{
-		margin: 7px 0;
-	}
-	.form-container {
-		flex:1;
-        padding-left:5%;
-  		text-align: center;
-		--font-size-base:1.2rem;
-	}
-
-	.socials{
-		font-size: 30px;
-		vertical-align: middle;
-	}
-
-	.socials-row{
-		vertical-align: middle;
-	}
-
-	*{box-sizing: border-box;}
-
-	.col-xs-12{
+	iframe {
 		width: 100%;
+		border-radius: 1rem;
+		border: var(--border);
 	}
 
-	.col-sm-6, .col-xs-12 {
-		min-height: 1px;
-		padding-left: 5px;
-		padding-right: 5px;
-
-	}
-	
-	.address-hours-container{
-		display:flex;
-		flex-direction: row;
-        padding-left:5%;
-  		text-align: left;
+	#inquiry-title {
+		text-transform: uppercase;
 	}
 
-	.address{
-		flex:1;
+	.container {
+		margin-top: 2rem;
+		display: flex;
+		flex-flow: row wrap;
+		align-content: center;
+		gap: 2rem;
 	}
 
-	.hours{
-		flex:1;
+	.embedded-map {
+		flex: 1;
+	}
+
+	.contact-flex-item {
+		display: flex;
+		flex: 1;
+		flex-flow: column;
+	}
+
+	input,
+	select {
+		flex: 1 1 33%;
+	}
+
+	textarea {
+		flex: 1 1 100%;
+		width: 100%;
+		min-height: 7rem;
+		max-height: 14rem;
+		resize: vertical;
+	}
+
+	.form-container {
+		flex: 1;
+		width: 100%;
+		height: 100%;
+		padding: 1rem;
+		text-align: center;
+		background: var(--color-text-card);
+		border-radius: 1rem;
+		border: var(--border);
+	}
+
+	.notify {
+		flex: 1 1 100%;
+		display: flex;
+		flex-flow: row wrap;
+		justify-content: center;
+		gap: 1rem;
+	}
+
+	.socials {
+		font-size: var(--font-size-base);
+		vertical-align: middle;
+	}
+
+	.socials-row {
+		vertical-align: middle;
+	}
+
+	.address-container {
+		padding: 1rem;
+		display: flex;
+		flex-flow: row wrap;
+		align-items: baseline;
 	}
 
 	/* SOCIAL MEDIA ICONS */
@@ -238,12 +259,12 @@
 		border-radius: 150%;
 	}
 	.fa-facebook {
-		background: #3B5998;
+		background: #3b5998;
 		color: white;
 	}
 
 	.fa-twitter {
-		background: #55ACEE;
+		background: #55acee;
 		color: white;
 	}
 
@@ -257,29 +278,10 @@
 		color: white;
 	}
 
-	#message{
-		width: 94%;
-		font-size: 1.2rem;
-	}
-
-	#submit-btn{
-		/* align-items: center; */
+	#submit-btn {
 		display: flex;
-		flex-direction: row;
+		flex: 1 1 100%;
 		justify-content: center;
+		width: 100%;
 	}
-
-	@media (min-width: 768px) {
-
-		input{
-			padding:18px;
-		}
-		.col-sm-6 {
-			width: 50%;
-			float:left;
-		}
-		
-	}
-
-
 </style>
