f33591ef2000b144fe9e0119f3cf9e774668538c
Alec Ames <dev@alecames.com>
2023-04-23 18:13:10 -0400

    Fixed mobile layout, updated about section

--- timeline/src/routes/about/+page.svelte
+++ timeline/src/routes/about/+page.svelte
@@ -2,70 +2,64 @@
 	import { slide } from "svelte/transition";
 	import PageTransition from "$lib/components/PageTransitionFly.svelte";
 
-	let members1 = [
+	let members = [
 		{
 			name: "Alec Ames",
 			pic: "https://media.licdn.com/dms/image/D5603AQGBaGdxymdLTA/profile-displayphoto-shrink_800_800/0/1679545357267?e=1687392000&v=beta&t=tdCHXPorM-cBHk8URmvTgFk83c2wrhPRO0cd0wFu-OA",
-			role: "Team Lead",
+			role: "Team Lead • Developer",
 			github: "https://github.com/alecames",
-			linkedin: "https://www.linkedin.com/in/alecames/"
+			linkedin: "https://www.linkedin.com/in/alecames/",
 		},
 		{
 			name: "Ibrahim Hashmi",
 			pic: "https://media.licdn.com/dms/image/C4E03AQEr70AZWiDNWA/profile-displayphoto-shrink_800_800/0/1613092600500?e=1687392000&v=beta&t=eV5-BPVA_HLEox8wmFLj4MJSPUe2gToHJKCAQ-d8b_o",
 			role: "Developer",
 			github: "https://github.com/ibhashmi",
-			linkedin: "https://www.linkedin.com/in/ibrahimh26/"
+			linkedin: "https://www.linkedin.com/in/ibrahimh26/",
 		},
 		{
 			name: "Francis Monwe",
 			pic: "https://media.licdn.com/dms/image/C4E03AQFheKji8LLI2g/profile-displayphoto-shrink_800_800/0/1652670840391?e=1687392000&v=beta&t=rykL51jyTdCAXCFHrkmR1bJaELjel8D91hUmYMJk7K8",
 			role: "Developer",
 			github: "https://github.com/monwe-jr",
-			linkedin: "https://www.linkedin.com/in/francis-monwe-674501198/"
+			linkedin: "https://www.linkedin.com/in/francis-monwe-674501198/",
 		},
 		{
 			name: "Matthew Benson",
 			pic: "https://media.licdn.com/dms/image/C4E03AQFk04BV0Phu4g/profile-displayphoto-shrink_800_800/0/1627429475282?e=1687392000&v=beta&t=8S9vHSFl1v565Hdm9EwftvrDOmUBH-1My5Neua5m5j8",
 			role: "Developer",
 			github: "https://github.com/MattMBenson",
-			linkedin: "https://www.linkedin.com/in/matthew-benson-59b1a31b7/"
-		
-		}
-	];
-
-	let members2 = [
+			linkedin: "https://www.linkedin.com/in/matthew-benson-59b1a31b7/",
+		},
 		{
 			name: "Tommy Pham",
 			pic: "https://media.licdn.com/dms/image/C4D03AQFyO7qwiqBjXQ/profile-displayphoto-shrink_800_800/0/1566411354394?e=1687392000&v=beta&t=GEGLxQY6epG9fB6DymGQlPMlPUeObS-zA948WMsP_P0",
 			role: "Developer",
 			github: "https://github.com/tommyphamca",
-			linkedin: "https://www.linkedin.com/in/tommyphamca/"
+			linkedin: "https://www.linkedin.com/in/tommyphamca/",
 		},
 		{
 			name: "Abhijeet Prajapati",
 			pic: "https://avatars.githubusercontent.com/u/68875359?v=4",
 			role: "Developer",
 			github: "https://github.com/TheDasher1",
-			linkedin: "https://www.linkedin.com/in/abhijeet-prajapati-4a20a7164/"
+			linkedin: "https://www.linkedin.com/in/abhijeet-prajapati-4a20a7164/",
 		},
 		{
 			name: "Justin Stickel",
 			pic: "https://media.licdn.com/dms/image/C5603AQHhT11yEwhjnw/profile-displayphoto-shrink_800_800/0/1608219438361?e=1687392000&v=beta&t=6zMLZCUaRMBFYvP4CSk_snY1a9PXHFpNOtcwlcD_0cw",
 			role: "Scrum Master",
 			github: "https://github.com/Stickelation",
-			linkedin: "https://www.linkedin.com/in/justin-stickel/"
+			linkedin: "https://www.linkedin.com/in/justin-stickel/",
 		},
 		{
 			name: "Haaris Yahya",
 			pic: "https://avatars.githubusercontent.com/u/80646420?v=4",
 			role: "Developer",
 			github: "https://github.com/haarisyahya",
-			linkedin: "https://www.linkedin.com/in/haaris-yahya-398ba9195/"
-		}
+			linkedin: "https://www.linkedin.com/in/haaris-yahya-398ba9195/",
+		},
 	];
-	
-
 </script>
 
 <svelte:head>
@@ -74,7 +68,7 @@
 </svelte:head>
 
 <PageTransition>
-	<gap/>
+	<gap />
 	<section class="page" in:slide>
 		<div>
 			<h1>About Niagara-on-the-Lake</h1>
@@ -111,48 +105,6 @@
 			</div>
 		</div>
 		<div>
-			<h1>Our Team</h1>
-			<div class="row v-align">
-				<!-- Team container  -->
-				<div class="container">
-				
-					<!-- {#each members as {name, pic, github}} -->
-					{#each members1 as member}
-						<div class="our-team">
-							<div class="avatar">
-								<img src={member.pic} alt="">
-							</div>
-							<div class="team-content">
-								<h3 class="title"> {member.name}</h3>
-								<span class="post">{member.role}</span>
-							</div>
-							<ul class="social">
-								<li><a href={member.linkedin} class="fa fa-linkedin-square"></a></li>
-								<li><a href={member.github} class="fa fa-github"></a></li>
-							</ul>
-						</div>
-					{/each}
-				</div>
-				<div class="container">
-					{#each members2 as member}
-						<div class="our-team">
-							<div class="avatar">
-								<img src={member.pic} alt="">
-							</div>
-							<div class="team-content">
-								<h3 class="title"> {member.name}</h3>
-								<span class="post">{member.role}</span>
-							</div>
-							<ul class="social">
-								<li><a href={member.linkedin} class="fa fa-linkedin-square"></a></li>
-								<li><a href={member.github} class="fa fa-github"></a></li>
-							</ul>
-						</div>
-					{/each}
-				</div>
-			</div>
-		</div>
-		<div>
 			<h1>About the Timeline</h1>
 			<div class="row v-align">
 				<p>
@@ -160,9 +112,10 @@
 					student developers from Brock University created for the
 					Niagara-on-the-Lake Museum. The project is an interactive timeline
 					that documents the history of NOTL from 1726 to the present day. The
-					timeline is developed for the Software Engineering II course taught by
-					Prof. Naser Ezzati-Jivan at Brock University. We designed this timeline
-					to be accessible for all users, on desktop and mobile devices.
+					timeline is developed as a term-long project for COSC 4P02 &mdash; the
+					Software Engineering course taught by Prof. Naser Ezzati-Jivan at
+					Brock University. We designed this timeline to be accessible for all
+					users, on desktop and mobile devices.
 				</p>
 				<img
 					width="100"
@@ -171,89 +124,96 @@
 					alt="Brock University Logo" />
 			</div>
 		</div>
+		<div>
+			<h1>Our Team</h1>
+			<div class="row v-align">
+				<p style="text-align:center">
+					Meet the software development team behind the Niagara-on-the-Lake
+					Timeline.
+				</p>
+				<!-- Team container  -->
+				<div class="container">
+					<!-- {#each members as {name, pic, github}} -->
+					{#each members as member}
+						<div class="member-card">
+							<div class="avatar">
+								<a href={member.github}
+									><img src={member.pic} alt={member.name} /></a>
+							</div>
+							<div class="team-content">
+								<h3 class="title">{member.name}</h3>
+								<span class="role">{member.role}</span>
+							</div>
+							<ul class="social">
+								<li>
+									<a href={member.linkedin}
+										><span class="fa fa-linkedin-square" /></a>
+								</li>
+								<li>
+									<a href={member.github}><span class="fa fa-github" /></a>
+								</li>
+							</ul>
+						</div>
+					{/each}
+				</div>
+			</div>
+		</div>
 	</section>
-	<big-gap/>
+	<big-gap />
 </PageTransition>
 
 <style>
-
-
-	.container{
-		display:flex;
-		flex-flow:row wrap;
-		align-items:center;
+	.container {
+		display: flex;
+		flex-flow: row wrap;
 		justify-content: center;
-		width: 80%;
-		margin:auto;
-
-		/* margin: 0; */
-		padding: 100px, 0px;
-		height: 100%;
-		box-sizing: border-box;
-		color:var(--color-text);
-		/* font-family: sans-serif; */
-		font-family: var(--font-sans);
+		margin: auto;
+		gap: 2rem;
 	}
 
-	.our-team{
-		flex:1;
-		/* background:#f7f5ec; */
-		background: var(--color-bg-2);
+	.member-card {
+		flex: 1 20%;
+		min-width: 16rem;
+		background: var(--color-bg-1);
+		padding: 1rem;
 		text-align: center;
 		align-items: center;
-		overflow: hidden;	
+		overflow: hidden;
 		position: relative;
-		margin: 0px 25px;
-		padding: 30px 70px;
 		border-radius: 1.5rem;
-		border:var(--border);
-		color: var(--color-text);
+		border: var(--border);
 	}
 
-	.our-team .avatar{
-		display:inline-block;
+	.member-card .avatar {
+		display: inline-block;
 		align-self: center;
-		width: 130px;
-		height: 130px;
+		width: 10rem;
+		height: 10rem;
 		margin-bottom: 0px;
 		z-index: 1;
 		position: relative;
-		/* position: center; */
 	}
 
-	.our-team .avatar::before{
+	.member-card .avatar::before {
 		content: "";
 		width: 100%;
 		height: 0;
-		border-radius: 50%;
-		background: #1785eb;
+		border-radius: 100%;
+		background: var(--color-theme-2);
 		position: absolute;
 		bottom: 135%;
 		right: 0;
 		left: 0;
 		opacity: 0.2;
 		transform: scale(3);
-		/* transition: all 0.3s linear 0s; */
-		transition: all 0.5s var(--curve);
+		transition: all 0.3s var(--curve);
 	}
 
-	.our-team:hover .avatar::before{
-		height:100%;
+	.member-card:hover .avatar::before {
+		height: 10rem;
 	}
 
-	.our-team .avatar:after{
-		content:"";
-		width:100%;
-		height: 0;
-		border-radius: 50%;
-		background: #1785eb;
-		position: absolute;
-		top:0;
-		left:0;
-		z-index:-1;
-	}
-
-	.our-team .avatar img{
+	.member-card .avatar img {
 		width: 100%;
 		height: auto;
 		border-radius: 50%;
@@ -261,59 +221,50 @@
 		transition: all 0.5s var(--curve);
 	}
 
-	.our-team:hover .avatar img{
-		box-shadow: 0 0 0 14px #f7f5ec;
+	.member-card:hover .avatar img {
+		box-shadow: 0 0 0 14px var(--color-theme-2);
 		transform: scale(0.9);
-
 	}
 
-	.our-team .team-content{
-		margin-bottom: 30px;
+	.member-card .team-content {
+		margin-bottom: 4rem;
 	}
 
-	.our-team .title{
-		font-size: 22px;
-		font-weight: 700;
-		/* color: #4e5052; */
-		letter-spacing: 1px;
-		margin-bottom: 5px;
+	.member-card .title {
+		font-size: var(--font-size-base);
 	}
 
-	.our-team .post{
+	.member-card .role {
 		display: block;
-		font-size: 15px;
-		
-
+		font-size: var(--font-size-small);
 	}
 
-	.our-team .social{
+	.member-card .social {
 		width: 100%;
 		padding: 0;
 		margin: 0;
-		background: #1785eb;
+		background: var(--color-theme-2);
 		position: absolute;
 		bottom: -100px;
 		left: 0;
 		transition: all 0.5s var(--curve);
 	}
 
-	.our-team:hover .social{
+	.member-card:hover .social {
 		bottom: 0;
 	}
 
-	.our-team .social li{
+	.member-card .social li {
 		display: inline-block;
-
 	}
 
-	.our-team .social li a{
+	.member-card .social li a {
 		text-decoration: none;
-		display:block;
-		padding: 10px;
-		font-size: 17px;
+		display: inline-flex;
+		padding: 0.7rem;
+		font-size: var(--font-size-small);
 		color: #fff;
-		transition: all 0.5s var(--curve);
-	
+		transition: all 0.3s var(--curve);
 	}
 
 	h1 {
@@ -349,17 +300,4 @@
 	.row {
 		gap: 3rem;
 	}
-
-	@media only screen and (max-width: 768px) {
-		/* For mobile phones: */
-		
-
-	/* .our-team:hover .avatar img{
-		overflow: visible;
-
-	} */
-		
-	}
-
-
 </style>
